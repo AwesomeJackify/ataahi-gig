b3dd8d7e46ada521e8edb9afd72545406ec3db46
Jack Gong <jackgong48@gmail.com>
2023-11-28 09:10:47 +0700

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,61 @@
+import logo from "../assets/images/logo.png";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
+
+interface Props {
+  pages: NavItem[];
+}
+
+const Header = ({ pages }: Props) => {
+  const [showNav, setShowNav] = useState(false);
+
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
+
+  return (
+    <div className="mb-10 max-md:mb-4">
+      <div className="flex justify-between items-center max-md:flex-col gap-6">
+        <div className="flex justify-between max-md:w-full items-center">
+          <a href="/" className="btn btn-ghost p-4 h-fit">
+            <img
+              src={logo.src}
+              alt="logo"
+              width={500}
+              className="max-md:w-40 w-52"
+            />
+          </a>
+
+          {!showNav ? (
+            <AiOutlineMenu
+              className="max-md:block hidden text-5xl cursor-pointer"
+              onClick={toggleNav}
+            />
+          ) : (
+            <AiOutlineClose
+              className="max-md:block hidden text-5xl cursor-pointer"
+              onClick={toggleNav}
+            />
+          )}
+        </div>
+        <div
+          className={`navbar bg-base-100 justify-end ${
+            showNav ? "max-md:block" : "max-md:hidden"
+          }`}
+        >
+          <ul
+            className={`menu menu-horizontal px-1 gap-6 max-md:w-full text-xl max-md:flex-col`}
+          >
+            {pages.map((page, index) => (
+              <li key={index}>
+                <a href={page.url}>{page.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
